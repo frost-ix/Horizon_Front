073db4ca590b419c4693d6da87a370f63f7849d0
frost-secu <shw9810@icloud.com>
2023-08-23 05:03:20 +0000

    change: target IP

--- src/setProxy.ts
+++ src/setProxy.ts
@@ -1,5 +1,5 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 export default module.exports = function (app: any) {
-  app.use(createProxyMiddleware("/", { target: "http://localhost:3100", changeOrigin: true }));
+  app.use(createProxyMiddleware("/", { target: "http://172.17.0.1:3100", changeOrigin: true }));
 };
