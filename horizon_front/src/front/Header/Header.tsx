329a9d7131f56ef417dd14a501a7f9c59b2e4f74
jungsonghun <h1jshmal1@naver.com>
2024-01-21 14:16:37 +0000

    프론트 추가

---
+++ horizon_front/src/front/Header/Header.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import './Header.css';
+
+function Header() {
+    const [displayLine, setDisplayLine]=useState<boolean>(false);
+  return (
+    <div className="Header">
+         {!displayLine && (
+            <img src="/Icon/DisplayLine.jpeg" className='displayLineIcon' onClick={()=>{setDisplayLine(!displayLine)}}/>
+        )}
+        
+        {displayLine && (
+            <div className='displayBar'>
+                <div className='displayBar-menu-first'>
+                    <img src="/Icon/DisplayLine.jpeg" className='displayLineIcon' onClick={()=>{setDisplayLine(!displayLine)}}/>
+                </div>
+                
+                <div className='displayBar-menu-second'>
+                    메뉴
+                </div>
+            </div>
+        )}
+    </div>
+  );
+}
+
+export default Header;
