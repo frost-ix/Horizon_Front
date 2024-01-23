076cd27a94a70f317f7d47cf1a39e633ae3d3b91
jungsonghun <h1jshmal1@naver.com>
2024-01-23 14:38:46 +0000

    2024-01-23

--- horizon_front/src/front/Header/Header.tsx
+++ horizon_front/src/front/Header/Header.tsx
@@ -1,31 +1,59 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Header.css';
+import { useNavigate } from "react-router-dom";
 
 function Header() {
-    const [displayLine, setDisplayLine]=useState<boolean>(false);
-  return (
-    <div className="Header">
-         {!displayLine && (
-            <img src="/Icon/DisplayLine.jpeg" className='displayLineIcon' onClick={()=>{setDisplayLine(!displayLine)}}/>
-        )}
-        
-        {displayLine && (
-            <div className='displayBar'>
+    const navigate = useNavigate();
+    const [displayLine, setDisplayLine] = useState<boolean>(false);
+
+    const displayBarClick = () => {
+        setDisplayLine(!displayLine);
+    };
+    const AccountClick = () => {
+        navigate('/account')
+    };
+    const PostClick = () => {
+        navigate('/post')
+    };
+    const MainClick = () => {
+        navigate('/')
+    };
+    const SearchClick = () => {
+        // navigate('/search')
+        alert("기능 준비 중")
+    };
+    const BookClick = () => {
+        navigate('/book')
+    };
+
+    return (
+        <div className="Header">
+            <div className='Header-icons'>
+                <img src="/Icon/DisplayLine.png" className='displayLineIcon' onClick={displayBarClick} alt="icon"/>{/* 컴퓨터 */}
+                <img src="/Icon/Search.png"  className='AccountIcon' onClick={SearchClick} alt="icon"/>
+                <img src="/Icon/Book.png"  className='AccountIcon' onClick={BookClick} alt="icon"/>
+                <img src="/Icon/Home.png"  className='AccountIcon' onClick={MainClick} alt="icon"/>
+                <img src="/Icon/Post.png"  className='AccountIcon' onClick={PostClick} alt="icon"/>
+                <img src="/Icon/Account.png"  className='AccountIcon' onClick={AccountClick} alt="icon"/>
+            </div>
+
+            {/* 컴퓨터 */}
+            <div className={`displayBar ${displayLine ? 'visible' : ''}`}>
                 <div className='displayBar-menu-first'>
-                    <img src="/Icon/DisplayLine.jpeg" className='displayLineIcon' onClick={()=>{setDisplayLine(!displayLine)}}/>
+                  <img src="/Icon/DisplayLine.png" className='displayLineIcon' onClick={displayBarClick} alt="icon"/>
                 </div>
-                
-                <div className='iwantcenter'>
-                    <div className='displayBar-menu-border'/>
+
+                <div className='iwantCenter'>
+                    <div className='displayBar-menu-border' />
+                </div>
+
+                <div className='displayBar-menu-second'>
                     
-                    <div className='displayBar-menu-second'>
-                        메뉴
-                    </div>
                 </div>
             </div>
-        )}
-    </div>
-  );
+
+        </div>
+    );
 }
 
 export default Header;
---
+++ horizon_front/src/front/Main/Main.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Main.css';
+
+import BoardList from './Board/BoardList';
+
+function Main() {
+  return (
+    <div className="Main">
+        <BoardList/>
+    </div>
+  );
+}
+
+export default Main;
