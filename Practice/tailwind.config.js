ab49c55ff2d98e471b975c5fe6142633e3b7ce72
frost-ix <shw9810@icloud.com>
2024-01-26 15:47:02 +0900

    fix : separate folder & set main folder <Horizon_front>

--- Practice/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: "class", // or 'media' or 'class'
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
