19a38b1fffe0325fbf3ff48fb0cc98b4b9c76f60
ehdrjs0110 <ehdrjs0110@naver.com>
2024-03-22 01:55:47 +0900

    Approval Back Connect

--- src/front/Main/manager/Approval.tsx
+++ src/front/Main/manager/Approval.tsx
@@ -1,16 +1,48 @@
 import React, { useState , useRef , useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import './approval.css';
 
 
+
 const Approval = () => {
+
+  interface UserData {
+    userId: string;
+    name: string;
+    studentId: string;
+    department: string;
+    phone: string;
+    email: string;
+    photo: string;
+  }
+
+  const [data, setData] = useState<UserData[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3100/approval/users", {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        if(res.data !== undefined){
+          setData(res.data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
   
   // 리스트 데이터
-  const data = [
-    { check: 0, id: 'hoseo123', name: '김호서', studentid: '2025201012', phone: '010-1234-5678', email: 'hoseo123@naver.com', photo: 'test1.jpg'},
-    { check: 1, id: 'nohappy34', name: '노행복', studentid: '2025201031', phone: '010-1111-2222', email: 'nohappy34@gmail.com', photo: 'test2.png'},
-    { check: 2, id: 'silverstar1101', name: '이은별', studentid: '2025202015', phone: '010-1101-1101', email: 'silverstar1101@naver.com', photo: 'test3.jpg'}
-  ];
+  // const data = [
+  //   { check: 0, userId: 'hoseo123', name: '김호서', studentId: '2025201012', department: 'ICT융합보안', phone: '010-1234-5678', email: 'hoseo123@naver.com', photo: 'test1.jpg'},
+  //   { check: 1, userId: 'nohappy34', name: '노행복', studentId: '2025201031', department: '호텔조리', phone: '010-1111-2222', email: 'nohappy34@gmail.com', photo: 'test2.png'},
+  //   { check: 2, userId: 'silverstar1101', name: '이은별', studentId: '2025202015', department: '웹툰애니메이션', phone: '010-1101-1101', email: 'silverstar1101@naver.com', photo: 'test3.jpg'}
+  // ];
   
   //Modal
   const [isShow, setShow] = useState(false);
@@ -19,7 +51,7 @@ const Approval = () => {
   // 체크박스 선택
   const [checkItems, setCheckItems] = useState<any[]>([]);
 
-  const handleSingleCheck = (checked : boolean, check : number) => {
+  const handleSingleCheck = (checked : boolean, check : string) => {
     if (checked) {
       // 단일 선택 시 체크된 아이템을 배열에 추가
       setCheckItems(prev => [...prev, check]);
@@ -33,7 +65,7 @@ const Approval = () => {
     if(checked) {
       // 전체 선택 클릭 시 데이터의 모든 아이템(id)를 담은 배열로 checkItems 상태 업데이트
       const idArray : any = [];
-      data.forEach((el) => idArray.push(el.check));
+      data.forEach((el) => idArray.push(el.userId));
       setCheckItems(idArray);
     }
     else {
@@ -47,7 +79,7 @@ const Approval = () => {
       //alert("승인되었습니다.");
       if(checkItems){
         checkItems.forEach((checked) => {
-          const account = data.find(el => el.check === checked);
+          const account = data.find(el => el.userId === checked);
           console.table(account);
         });
         alert("승인되었습니다.");
@@ -59,7 +91,7 @@ const Approval = () => {
     if (window.confirm("삭제 하시겠습니까?")) {
       if(checkItems){
         checkItems.forEach((checked) => {
-          const account = data.find(el => el.check === checked);
+          const account = data.find(el => el.userId === checked);
           console.table(account);
         });
         alert("삭제되었습니다.");
@@ -89,6 +121,7 @@ const Approval = () => {
                 <th className='th-id'>아이디</th>
                 <th className='th-name'>이름</th>
                 <th className='th-studentid'>학번</th>
+                <th className='th-department'>학과</th>
                 <th className='th-phone'>전화번호</th>
                 <th className='th-email'>이메일</th>
                 <th className='th-photo'>사진</th>
@@ -97,13 +130,14 @@ const Approval = () => {
             <tbody>
             {data.map((item, index) => (
               <tr key={index}>
-                <td><input type='checkbox' id={`check-${index}`} name={`select-${item.check}`}
-                  onChange={(e) => handleSingleCheck(e.target.checked, item.check)}
+                <td><input type='checkbox' id={`check-${index}`} name={`select-${item.userId}`}
+                  onChange={(e) => handleSingleCheck(e.target.checked, item.userId)}
                   // 체크된 아이템 배열에 해당 아이템이 있을 경우 선택 활성화, 아닐 시 해제
-                  checked={checkItems.includes(item.check)} /></td>
-                <td>{item.id}</td>
+                  checked={checkItems.includes(item.userId)} /></td>
+                <td>{item.userId}</td>
                 <td>{item.name}</td>
-                <td>{item.studentid}</td>
+                <td>{item.studentId}</td>
+                <td>{item.department}</td>
                 <td>{item.phone}</td>
                 <td>{item.email}</td>
                 <td><button onClick={(e) => showPhoto(item.photo)}>사진보기</button></td>
